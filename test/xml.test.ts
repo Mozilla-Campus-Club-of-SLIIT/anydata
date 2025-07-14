d352f87c586d6325b2f4939f9ab833828d8160f2
Seniru <senirupasan@gmail.com>
2025-07-14 22:06:21 +0530

    test: update test cases to align with data getter

--- test/xml.test.ts
+++ test/xml.test.ts
@@ -45,10 +45,9 @@ describe("xml", () => {
 
     it("should create a StructuredData object with a valid xml file", async () => {
       const data = await xml.loadFile("test.xml")
-
       assert.ok(data instanceof StructuredData)
       // todo: better test cases once we are done with the data getter for xml
-      assert.equal((data.data as Record<string, any>)["library"].length, 2)
+      assert.equal((data.data as Record<string, any>).library.books.length, 2)
       assert.strictEqual(data.originFormat, "xml")
     })
 
@@ -100,29 +99,68 @@ describe("xml", () => {
       const data = xml.from(cdataXml)
 
       assert.ok(data instanceof StructuredData)
-      assert.strictEqual(
-        (data.data as Record<string, any>)["root"][0].item,
-        "Some <unescaped> content",
-      )
+      assert.strictEqual((data.data as Record<string, any>).root.item, "Some <unescaped> content")
     })
 
     it("should handle attributes with special characters", () => {
       const attrXml = `<root><item id="item1" name="Test &amp; Example">Content</item></root>`
       const data = xml.from(attrXml)
       assert.ok(data instanceof StructuredData)
-      const item = (data.data as Record<string, any>)["root"][0].item
+      const item = (data.data as Record<string, any>).root.item
       assert.strictEqual(item.id, "item1")
       assert.strictEqual(item.name, "Test & Example")
-      assert.strictEqual(item.value, "Content")
+      assert.strictEqual(item.$value, "Content")
     })
 
     it("should handle empty elements", () => {
       const emptyXml = `<root><item id="empty" /></root>`
       const data = xml.from(emptyXml)
       assert.ok(data instanceof StructuredData)
-      const item = (data.data as Record<string, any>)["root"][0].item
+      const item = (data.data as Record<string, any>).root.item
       assert.strictEqual(item.id, "empty")
       assert.strictEqual(item.value, undefined) // Empty element should not have a value
     })
+
+    it("should handle nested elements", () => {
+      const nestedXml = `<root><parent><child>Content</child></parent></root>`
+      const data = xml.from(nestedXml)
+      assert.ok(data instanceof StructuredData)
+      const parent = (data.data as Record<string, any>).root.parent
+      assert.strictEqual(parent.child, "Content")
+    })
+
+    it("should handle elements with multiple attributes", () => {
+      const multiAttrXml = `<root><item id="item1" name="Test" value="Example" /></root>`
+      const data = xml.from(multiAttrXml)
+      assert.ok(data instanceof StructuredData)
+      const item = (data.data as Record<string, any>).root.item
+      assert.strictEqual(item.id, "item1")
+      assert.strictEqual(item.name, "Test")
+      assert.strictEqual(item.value, "Example")
+    })
+
+    // this is more of a StrucuturedData test, I'll leave it here so we can copy paste it later
+    /*
+    it("it should group common items", () => {
+      const groupedXml = `<root>
+        <item>
+          <name>Item 1</name>
+          <value>Value 1</value>
+        </item>
+        <item>
+          <name>Item 2</name>
+          <value>Value 2</value>
+        </item>
+      </root>`
+      const data = xml.from(groupedXml)
+      assert.ok(data instanceof StructuredData)
+      const items = (data.data as Record<string, any>).root.items
+      assert.strictEqual(items.length, 2)
+      assert.strictEqual(items[0].name, "Item 1")
+      assert.strictEqual(items[0].value, "Value 1")
+      assert.strictEqual(items[1].name, "Item 2")
+      assert.strictEqual(items[1].value, "Value 2")
+    })
+    */
   })
 })
